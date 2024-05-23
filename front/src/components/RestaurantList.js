82c38dd29df73df1ecd38b5611b80b391b672166
TaniaBar <taniabarillaro1@gmail.com>
2024-05-23 09:58:51 +0200

    add dossier components and file RestaurantList.js

---
+++ front/src/components/RestaurantList.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect } from 'react';
+import axios from 'axios';
+
+const RestaurantList = () => {
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(() => {
+        axios.get('/restaurant')
+        .then(response => setRestaurants(response.data))
+        .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Restaurant List</h1>
+            <ul>
+                {restaurants.map(restaurant => (
+                    <li key={restaurants.id}>{restaurants.name} - {restaurants.city}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default RestaurantList;
